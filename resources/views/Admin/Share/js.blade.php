26b39110093fc5f4f457b7e62fab287814399db7
TruongVanThuong <tvt230101@gmail.com>
2024-03-21 01:39:42 +0700

    cai thu vien va tao ca chuc nang crud

---
+++ resources/views/Admin/Share/js.blade.php
@@ -0,0 +1,27 @@
+<script src="/assets_admin/js/bootstrap.bundle.min.js"></script>
+<!--plugins-->
+<script src="/assets_admin/js/jquery.min.js"></script>
+<script src="/assets_admin/plugins/simplebar/js/simplebar.min.js"></script>
+<script src="/assets_admin/plugins/metismenu/js/metisMenu.min.js"></script>
+<script src="/assets_admin/plugins/perfect-scrollbar/js/perfect-scrollbar.js"></script>
+<script src="/assets_admin/plugins/vectormap/jquery-jvectormap-2.0.2.min.js"></script>
+<script src="/assets_admin/plugins/vectormap/jquery-jvectormap-world-mill-en.js"></script>
+
+{{-- <script src="/assets_admin/js/index.js"></script> --}}
+<!--app JS-->
+<script src="/assets_admin/js/app.js"></script>
+{{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script> --}}
+<script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.4.0/axios.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.4/moment.min.js"></script>
+
+<!-- validation jquery -->
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.5/jquery.validate.min.js"></script>
+<!-- <script src="/public/ckeditor/ckeditor.js"></script> -->
+<!-- bootstrap modal -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+<script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
+
